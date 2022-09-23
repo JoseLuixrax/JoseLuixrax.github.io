d5749c806622ecc9657fdca2e66b215a479faedd
JoseLuixrax <91657266+JoseLuixrax@users.noreply.github.com>
2022-09-23 14:42:22 +0200

    Add files via upload

---
+++ suma.php
@@ -0,0 +1,5 @@
+<?php
+$num1 = 5;
+$num2 = 8;
+echo("La suma de $num1+$num2 es: ". $num1+$num2 . "<br>");
+echo("Repositorio con el código <a href='https://github.com/JoseLuixrax/JoseLuixrax.github.io' target=_blank>aqui</a>");
