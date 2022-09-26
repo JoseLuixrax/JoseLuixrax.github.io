6c4edae171153ed001bf066653c08605657161b7
JoseLuixrax <91657266+JoseLuixrax@users.noreply.github.com>
2022-09-26 09:14:15 +0200

    Add files via upload

--- holaMundo.php
+++ holaMundo.php
@@ -1,3 +1,7 @@
 <?php
+/**
+ * @author Jose Luis Pérez Lara
+ * este script muestra un hola mundo
+ */
 echo'"Hola Mundo"';
-echo'<br>Enlace a <a href=https://github.com/JoseLuixrax/JoseLuixrax.github.io/blob/main/holaMundo.php>GitHub</a>';
+echo'<br>Enlace a <a href=https://github.com/JoseLuixrax/JoseLuixrax.github.io/blob/main/holaMundo.php target=_blank>GitHub</a>';
